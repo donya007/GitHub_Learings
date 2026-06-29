993abcbd4a9d1c67c822f028b08dbfe308290a55
Ezaz Ali Arshad <ezaz@gmail.com>
2026-06-29 23:49:30 +0530

    provider, rg add

---
+++ .github/workflows/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    source = "hashicorp/azurerm"
+    version = "4.68.8"
+    
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "ddece533-2a97-4b73-8056-67abfc878c00"
+  
+}
---
+++ .github/workflows/rg.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name = "Ezaz-GitHubAction-rg"
+  location = "Central India"
+}
