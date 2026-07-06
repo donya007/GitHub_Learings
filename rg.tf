95dc7ca47b2012f7d145a00994bb91e5ae362dd4
Ezaz Ali Arshad <ezaz@gmail.com>
2026-07-06 19:35:02 +0530

    rg,provider, storage delete

--- rg.tf
+++
@@ -1,4 +0,0 @@
-# resource "azurerm_resource_group" "rg" {
-#   name = "Ezaz-GitHubAction-1-rg"
-#   location = "Central India"
-# }
--- storage.tf
+++
@@ -1,9 +0,0 @@
-# resource "azurerm_storage_account" "sa" {
-#   depends_on = [azurerm_resource_group.rg]
-#   name = "ezazsa"
-#   location = "Central India"
-#   resource_group_name = "Ezaz-GitHubAction-rg"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"  
-# }
-
